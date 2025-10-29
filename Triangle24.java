b0dc845429e15c6ddafc0afa2d4126fffacd2365
reismaa <apriliandv07@gmail.com>
2025-10-29 19:54:10 +0700

    perbaikan

--- Triangle24.java
+++ Triangle24.java
@@ -14,6 +14,7 @@ public class Triangle24 {
                 System.out.print("*");
                 j++;
             }
+            System.out.println(); // perbaikan
             i++;
         }
 
